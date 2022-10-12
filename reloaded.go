af49a09e745b045e7d18c65588631d14649bbf21
choang <charhoang@outlook.com>
2022-10-12 19:00:38 +0100

    reloaded

--- reloaded.go
+++ reloaded.go
@@ -8,19 +8,44 @@ import (
 	"strings"
 )
 
+// read sample txt, apply functions
 func main() {
-	content, err := os.ReadFile("sample.txt")
+	input, err := os.ReadFile("sample.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	strContent := string(content)
-	strContent1 := Separate(strContent)
+	str := string(input)
+	strContent := strings.Split(str, " ")
 }
 
-func Separate(content string) []string {
-	return strings.Split(content, " ")
+func convertString(strContent []string) []string {
+	var emptyString []string
+	for i := 0; i < len(strContent); i++ {
+		switch strContent[i] {
+		case "(hex)":
+			convHex, _ := strconv.ParseInt(strContent[i-1], 16, 64)
+			emptyString[len(emptyString)-1] = fmt.Sprint(convHex)
+		case "(bin)":
+			convBin, _ := strconv.ParseInt(strContent[i-1], 2, 64)
+			emptyString[len(emptyString)-1] = fmt.Sprint(convBin)
+		case "(cap)":
+			convCap := strings.Title(strContent[i-1])
+			emptyString[len(emptyString)-1] = fmt.Sprint(convCap)
+		case "(up)":
+			convUp := strings.ToUpper(strContent[i-1])
+			emptyString[len(emptyString)-1] = fmt.Sprint(convUp)
+		case "(low)":
+			convLow := strings.ToLower(strContent[i-1])
+			emptyString[len(emptyString)-1] = fmt.Sprint(convLow)
+		default:
+			return emptyString
+		}
+	}
+	return emptyString
 }
 
+/*
+// check for punctuation (except apostrophes)
 func checkPunct(content []string) bool {
 	for _, content := range content {
 		switch content {
@@ -31,10 +56,51 @@ func checkPunct(content []string) bool {
 	return false
 }
 
+func checkPunctGrp(content []string) bool {
+	for _, content := range content {
+		switch content {
+		case "!?", "?!", "...":
+			return true
+		}
+	}
+	return false
+}
+
+/*
+func fixPunct(content []string) []string {
+	emptyString := ""
+	// checks for punctuation
+	for i := 0; i < len(strContent1); i++ {
+		checkPunct(strContent1[i])
+		checkPunctGrp(strContent1[i])
+		// i = punctuation, check if i-1 is a whitespace
+		if checkPunct(strContent1[i]) == true || checkPunctGrp(strContent[i]) == true {
+			for j := i; j < len(strContent1); j++ {
+				if strContent1[i-1] == " " {
+					strContent1[i-1] = strContent[i-2]
+					return strContent[i-1]
+				} else {
+					return strContent[i]
+				}
+			}
+		} else {
+			continue
+		}
+	}
+}
+*/
+/*
+// check hex, return decimal
 func Hex(s string) int64 {
 	decimal, err := strconv.ParseInt(s, 16, 32)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return decimal
+}
+*/
+func Cap() {
+	str := "hello world"
+	capStr := strings.Title(str)
+	fmt.Println(capStr)
 }
--- test/test1.go
+++ test/test1.go
@@ -2,24 +2,75 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"strings"
 )
 
-/*
-func main() {
-	str := "Hello world, good morning"
-	separatedStr := strings.Fields(str)
-	for _, separatedStr := range separatedStr {
-		fmt.Println(separatedStr)
+func convertString(strContent []string) []string {
+	var emptyString []string
+	for i := 0; i < len(strContent); i++ {
+		switch strContent[i] {
+		case "(hex)":
+			convHex, _ := strconv.ParseInt(strContent[i-1], 16, 64)
+			emptyString[len(emptyString)-1] = fmt.Sprint(convHex)
+		case "(bin)":
+			convBin, _ := strconv.ParseInt(strContent[i-1], 2, 64)
+			emptyString[len(emptyString)-1] = fmt.Sprint(convBin)
+		case "(cap)":
+			convCap := strings.Title(strContent[i-1])
+			emptyString[len(emptyString)-1] = fmt.Sprint(convCap)
+		case "(up)":
+			convUp := strings.ToUpper(strContent[i-1])
+			emptyString[len(emptyString)-1] = fmt.Sprint(convUp)
+		case "(low)":
+			convLow := strings.ToLower(strContent[i-1])
+			emptyString[len(emptyString)-1] = fmt.Sprint(convLow)
+		default:
+			emptyString = append(emptyString, strContent[i])
+		}
 	}
+	return emptyString
 }
-*/
+
+func checkPunct(strContent []string) bool {
+	for i := 0; i < len(strContent); i++ {
+		switch strContent[i] {
+		case ".", ",", "!", "?", ":", ";":
+			return true
+		default:
+			return false
+		}
+	}
+	return true
+}
+
+func movePunct(strContent []string) string {
+	for i := 0; i < len(strContent); i++ {
+		if checkPunct(strContent) {
+			if strContent[i-1] == " " {
+				strContent[i-1] = strContent[i]
+				return strContent[i]
+			}
+		}
+	}
+	return strContent[i]
+}
 
 func main() {
-	str := "it was (cap, 2) the , best of times,"
-	delimiter1 := "("
-	delimiter2 := ")"
-	parts1 := strings.Split(str, delimiter1)
-	parts2 := strings.Split(string(parts1), delimiter2)
-	fmt.Println(parts2)
+	args := os.Args
+	input, err := os.ReadFile(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := string(input)
+	strContent := strings.Split(str, " ")
+	converted := convertString(strContent)
+	fmt.Println(strContent)
+	fmt.Println(converted)
+
+	movePunctStr := movePunct(strContent)
+	fmt.Println(movePunctStr)
 }
