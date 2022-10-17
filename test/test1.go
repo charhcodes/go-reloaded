35e2a518ee655e24dde907d1e8cc92a92df0e214
choang <choang@outlook.com>
2022-10-17 16:26:08 +0100

    reloaded

--- test/test1.go
+++ test/test1.go
@@ -21,19 +21,53 @@ func convertString(strContent []string) []string {
 		case "(cap)":
 			convCap := strings.Title(strContent[i-1])
 			emptyString[len(emptyString)-1] = fmt.Sprint(convCap)
+		case "(cap,":
+			deleteBracket := strings.Trim(strContent[i+1], ")")
+			strToInt, err := strconv.Atoi(deleteBracket)
+			if err != nil {
+				panic(err)
+			}
+			for j := 1; j <= strToInt; j++ {
+				convCap := strings.Title(emptyString[len(emptyString)-j])
+				emptyString[len(emptyString)-j] = fmt.Sprint(convCap)
+			}
+			i++
 		case "(up)":
 			convUp := strings.ToUpper(strContent[i-1])
 			emptyString[len(emptyString)-1] = fmt.Sprint(convUp)
+		case "(up,":
+			deleteBracket := strings.Trim(strContent[i+1], ")")
+			strToInt, err := strconv.Atoi(deleteBracket)
+			if err != nil {
+				panic(err)
+			}
+			for j := 1; j <= strToInt; j++ {
+				convUp := strings.ToUpper(emptyString[len(emptyString)-j])
+				emptyString[len(emptyString)-j] = fmt.Sprint(convUp)
+			}
+			i++
 		case "(low)":
 			convLow := strings.ToLower(strContent[i-1])
 			emptyString[len(emptyString)-1] = fmt.Sprint(convLow)
+		case "(low,":
+			deleteBracket := strings.Trim(strContent[i+1], ")")
+			strToInt, err := strconv.Atoi(deleteBracket)
+			if err != nil {
+				panic(err)
+			}
+			for j := 1; j <= strToInt; j++ {
+				convLow := strings.ToLower(emptyString[len(emptyString)-j])
+				emptyString[len(emptyString)-j] = fmt.Sprint(convLow)
+			}
+			i++
 		default:
 			emptyString = append(emptyString, strContent[i])
 		}
+		return emptyString
 	}
-	return emptyString
 }
 
+// check punctuation (except apostrophes)
 func checkPunct(strContent []string) bool {
 	for i := 0; i < len(strContent); i++ {
 		switch strContent[i] {
@@ -46,17 +80,28 @@ func checkPunct(strContent []string) bool {
 	return true
 }
 
-func movePunct(strContent []string) string {
-	for i := 0; i < len(strContent); i++ {
-		if checkPunct(strContent) {
-			if strContent[i-1] == " " {
-				strContent[i-1] = strContent[i]
-				return strContent[i]
-			}
-		}
-	}
-	return strContent[i]
-}
+// // check punctuation group
+// func checkPunctGrp(content []string) bool {
+// 	for _, content := range content {
+// 		switch content {
+// 		case "!?", "?!", "...":
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// // move punctuation
+// func movePunct(strContent []string) []string {
+// 	for i := 0; i < len(strContent); i++ {
+// 		if checkPunct(strContent) {
+// 			if strContent[i-1] == " " {
+// 				strContent[i-1] = fmt.Sprint(strContent[i])
+// 			}
+// 		}
+// 	}
+// 	return strContent
+// }
 
 func main() {
 	args := os.Args
